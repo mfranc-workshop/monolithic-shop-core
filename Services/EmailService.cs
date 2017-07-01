c2acb4df55afb6d00adb5bad79e5532e430eb080
Michal Franc <lam.michal.franc@gmail.com>
2017-07-01 13:30:42 +0100

    Changed the way we serialize the objects to user Json

--- Services/EmailService.cs
+++ Services/EmailService.cs
@@ -5,6 +5,7 @@ using RestEase;
 using System.Threading.Tasks;
 using RawRabbit;
 using RawRabbit.vNext;
+using Newtonsoft.Json;
 
 namespace monolithic_shop_core.Services
 {
@@ -80,9 +81,12 @@ namespace monolithic_shop_core.Services
         {
             try
             {
+                var msg = new EmailMessage(emailAddress, Email.Templates[type].Item2);
+                var jsonData = JsonConvert.SerializeObject(msg);
+
                 _logger.Info($"Sending email command");
-                await _client.PublishAsync(new Tuple<string, string>(emailAddress, Email.Templates[type].Item2), default(Guid), 
-                cfg => cfg.WithExchange(ex => ex.WithName("email_exchange")));
+                await _client.PublishAsync(jsonData, default(Guid),
+                cfg => cfg.WithExchange(ex => ex.WithName("email_exchange")).WithRoutingKey("email_command"));
                 _logger.Info($"Email request sent - {emailAddress} of type - {type}");
             }
             catch(Exception ex)
